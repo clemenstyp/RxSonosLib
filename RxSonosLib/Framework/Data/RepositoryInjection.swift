d0591b6b99bee0abc1a2df448da6866a4fddd0c9
Clemens Putschli <clemens@putschli.de>
2018-10-13 09:20:03 +0200

    removed cache reset

--- RxSonosLib/Framework/Data/RepositoryInjection.swift
+++ RxSonosLib/Framework/Data/RepositoryInjection.swift
@@ -13,7 +13,7 @@ class RepositoryInjection {
     internal static let shared: RepositoryInjection = RepositoryInjection()
     
     init() {
-        CacheManager.shared.deleteAll()
+        //CacheManager.shared.deleteAll()
     }
     
     internal var ssdpRepository: SSDPRepository = SSDPRepositoryImpl()
